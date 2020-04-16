8e4add69b32c03a1086a7508e167a1e989eeaa9b
AjvorHardi <markovic.goran@outlook.com>
2020-04-16 22:49:37 +0200

    Vracene rute pod midlver

--- routes/api.php
+++ routes/api.php
@@ -18,26 +18,26 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('gradebooks', 'GradebookController');
-
-Route::resource('teachers', 'TeacherController');
-
-Route::get('/gradebooks/{gradebook_id}/comments', ['as' => 'gradebook-comments', 'uses' => 'CommentController@index']);
-
-Route::post('/gradebooks/{gradebook_id}/comments', ['as' => 'gradebook-comments-add', 'uses' => 'CommentController@store']);
-
-Route::delete('/gradebooks/{gradebook_id}/comments/{comment_id}', ['as' => 'gradebook-comments-remove', 'uses' => 'CommentController@destroy']);
-
-Route::post('/gradebooks/{gradebook_id}/students/create', ['as' => 'gradebook-students-create', 'uses' => 'StudentController@store']);
-
-
-
-
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 
 Route::group(['middleware' => ['jwt.verify']], function() {
 
 
+    Route::resource('gradebooks', 'GradebookController');
+
+    Route::resource('teachers', 'TeacherController')->only([
+        'index', 'show', 'store'
+    ]);
+    
+    Route::get('/gradebooks/{gradebook_id}/comments', ['as' => 'gradebook-comments', 'uses' => 'CommentController@index']);
+    
+    Route::post('/gradebooks/{gradebook_id}/comments', ['as' => 'gradebook-comments-add', 'uses' => 'CommentController@store']);
+    
+    Route::delete('/gradebooks/{gradebook_id}/comments/{comment_id}', ['as' => 'gradebook-comments-remove', 'uses' => 'CommentController@destroy']);
+    
+    Route::post('/gradebooks/{gradebook_id}/students/create', ['as' => 'gradebook-students-create', 'uses' => 'StudentController@store']);
+    
+    
 
 });
